71143e31a73c7655762734c88de2ac82628683d5
Ben Reich <Ben Reich>
2019-12-08 14:09:02 -0500

    Minor fix for board sizes other than 4 and change solutions to iterator for efficiency

--- twentyfour.scala
+++ twentyfour.scala
@@ -1,7 +1,7 @@
 package twentyfour
 
 case class Board(nums: Int*) {
-	def solve = {
+	def solutions = {
 		val numCombos = nums.map(Fraction(_)).permutations.toList
 		val allOperators = Seq.fill(nums.length - 1)(Operator.all).flatten
 		val operatorCombos = allOperators.combinations(nums.length - 1).flatMap(_.permutations.toList).toList
@@ -13,12 +13,14 @@ case class Board(nums: Int*) {
 			operatorOrder <- operatorOrders
 		} yield Expression(nums, operators, operatorOrder) 
 
-		tries.filter(_.value == Rational(24))
+		tries.iterator.filter(_.value == Rational(24))
 	}
+
+	def solvable = solutions.nonEmpty
 }
 
 case object Board {
-	private def allBoards(n: Int) = Seq.fill(n)(1 to 13).flatten.combinations(4).map(nums => Board(nums:_*))
+	private def allBoards(n: Int) = Seq.fill(n)(1 to 13).flatten.combinations(n).map(nums => Board(nums:_*))
 
-	def noSolutions(n: Int) = allBoards(n).filter(_.solve.isEmpty)
+	def noSolutions(n: Int) = allBoards(n).filterNot(_.solvable)
 }
